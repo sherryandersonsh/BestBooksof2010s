794a59443d08611a02b0d978c77d9f3274ea5a23
sherryandersonsh <63329705+sherryandersonsh@users.noreply.github.com>
2021-09-07 01:13:59 -0600

    Update code to connect and pull data + pandas commands

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,23 @@
-import sqlalchemy
+import numpy as np
+import pandas as pd
 from sqlalchemy import create_engine
 
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/GR2010sBestBooks')
-    print(inspector.get_table_names()) # Lets you see the names of the tables present in the database
+# Connect and pull data from PostgreSQL and save to a dataframe(df)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/GR2010sBestBooks')
+connection = engine.connect()
+df = pd.read_sql('select * from books', connection)
+
+# Display all rows and columns in the dataframe
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+pd.set_option('max_colwidth', None)
+pd.options.display.width=None # Get data in a single line
+
+print(df[['title', 'author']].head(100))
+
+
+
+
 
 
