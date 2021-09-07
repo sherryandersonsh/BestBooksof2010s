ee44e0f0295b8b9762e86273dd7d67389100a5cc
sherryandersonsh <63329705+sherryandersonsh@users.noreply.github.com>
2021-09-06 22:39:34 -0600

    update

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,19 +1,7 @@
-import pandas as pd
 import sqlalchemy
-import psycopg2
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:P@localhost:5432/DataCamp_Courses')
 
-print(engine.table_names()) # Lets you see the names of the tables present in the database
-
-# Connecting to PostgreSQL
-def dbconnection():
-        connection = psycopg2.connect(user="postgres",
-                                      password="Polopo00!",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="GR2010sBestBooks")
-
-dialect+driver://username:password@host:port/database
+    engine = create_engine('postgresql://postgres:postgres@localhost:5432/GR2010sBestBooks')
+    print(inspector.get_table_names()) # Lets you see the names of the tables present in the database
 
 
